34b612296d61827a0caebb5871e742c4540ea8bf
boriss <boriss.djumajevs@npdit.com>
2020-01-17 22:58:06 +0200

    may be this is solution

--- client/js/controllers/welcomeController.js
+++ client/js/controllers/welcomeController.js
@@ -7,12 +7,16 @@ welcomeController.var2 = () => {
     return 'Output of function';
 }
 
+welcomeController.list = (el) => {
+    window['temp'] = el;
+    return welcomeController.compile(welcomeController.loadView('./views/welcome/listInList.html', false));
+}
+
 welcomeController.arr = [
-    'a',
-    'b',
-    'c'
+    ['a', 'b'],
+    ['c', 'd']
 ];
 
 welcomeController.isTitle = false;
 
-welcomeController.loadView('./views/welcome.html');
+welcomeController.loadView('./views/welcome/welcome.html');
