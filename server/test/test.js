ebaebfe62cbbbde5436f81589c5a7e11a232b7b3
Boriss <boriss.djumajevs@1clickgames.com>
2020-10-09 10:30:31 +0300

    new test

--- server/test/test.js
+++ server/test/test.js
@@ -1,5 +1,6 @@
 var assert = require('assert');
 var model = require('../kernel/model.js');
+var migration = require('../kernel/migration.js');
 
 describe('Model without db', function () {
     let d = [
@@ -48,7 +49,7 @@ describe('Model without db', function () {
     });
 
     describe('Update', function() {
-        it('Should return stutus = active', function() {
+        it('Should return status = active', function() {
             m.update(4, {
                 status: 'active'
             });
@@ -86,4 +87,118 @@ describe('Model without db', function () {
             assert.strictEqual(m.all().length, 100);
         });
     });
+});
+
+describe('Model and migrations with db', function () {
+    let config = {
+        host: "localhost",
+        user: "userName",
+        password: "password",
+        db: 'testDatabase'
+    }
+
+    //Migration
+    describe('Migration', function() {
+        var _migration = new migration({
+            host: config.host,
+            user: config.user,
+            password: config.password,
+            database: config.db,
+        }, true);
+        
+        _migration.migrate([
+            {table: 'testTable', drop: false, name: 'testColumn_first', type: 'int'},
+            {table: 'testTable', drop: false, name: 'testColumn_second', type: 'int'}
+        ]);
+    
+        let tableDescription = [ 
+            { Field: 'id',
+            Type: 'int',
+            Null: 'NO',
+            Key: 'PRI',
+            Default: null,
+            Extra: 'auto_increment' },
+        { Field: 'testColumn_first',
+            Type: 'int',
+            Null: 'YES',
+            Key: '',
+            Default: null,
+            Extra: '' },
+        { Field: 'testColumn_second',
+            Type: 'int',
+            Null: 'YES',
+            Key: '',
+            Default: null,
+            Extra: '' } 
+        ];
+
+        let tableDescription2 = [ 
+            { Field: 'id',
+            Type: 'int',
+            Null: 'NO',
+            Key: 'PRI',
+            Default: null,
+            Extra: 'auto_increment' },
+        { Field: 'testColumn_first',
+            Type: 'text',
+            Null: 'YES',
+            Key: '',
+            Default: null,
+            Extra: '' },
+        { Field: 'testColumn_second',
+            Type: 'text',
+            Null: 'YES',
+            Key: '',
+            Default: null,
+            Extra: '' } 
+        ];
+    
+        //Model
+        class testModel extends model {
+    
+        }
+        
+        m = new testModel ({
+            host: config.host,
+            user: config.user,
+            password: config.password,
+            database: config.db,
+            table: 'testTable',
+        });
+    
+        it('Check table after creation migration', function() {
+            assert.deepStrictEqual(m.describe(), tableDescription);
+        });
+
+        it('Check table after modify columns migration', function() {
+            _migration.migrate([
+                {table: 'testTable', drop: false, name: 'testColumn_first', type: 'text', action: 'MODIFY COLUMN'},
+                {table: 'testTable', drop: false, name: 'testColumn_second', type: 'text', action: 'MODIFY COLUMN'}
+            ]);
+
+            assert.deepStrictEqual(m.describe(), tableDescription2);
+        });
+    });
+
+    //Migration
+    describe('Model', function() {
+        describe('Insert', function() {
+            //TODO: fix empty rows
+            it('Should return new count of elements == 1', function() {
+                m.insert({testColumn_first: 'disable', testColumn_second: 'testMe'});
+                assert.strictEqual(m.all().length, 1);
+            });
+    
+            it('Should return new count of elements == 100', function() {
+                console.log(m.all())
+
+                for(let i = 0; i < 99; i++) {
+                    m.insert({testColumn_first: 'disable', testColumn_second: 'testMe'});                
+                }
+
+                assert.strictEqual(m.all().length, 100);
+            });
+        });
+    });
+  
 });
