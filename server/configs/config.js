6fcccec00360ab2977155853525782cb61d93e99
Boriss <boriss.djumajevs@1clickgames.com>
2021-04-09 18:12:43 +0300

    config

---
+++ server/configs/config.js
@@ -0,0 +1,14 @@
+//Settings for DB connection
+var configs = {
+    "mysql": {
+        host: "localhost",
+        user: "MyUser",
+        password: "MyPass",
+        db: "MyDb"
+    },
+    "socket": {
+        "port": 3030
+    }
+};
+
+module.exports = configs;
