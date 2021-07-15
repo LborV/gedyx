e04e25c3ad0e81b825fa1ddcc850b3654b9ecf74
Boriss <boriss.djumajevs@1clickgames.com>
2021-07-15 16:35:07 +0300

    delete old tests

--- server/test/test_mysqlInterface.js
+++ server/test/test_mysqlInterface.js
@@ -19,57 +19,5 @@ describe('Model and migrations with db', function () {
         database: config.db,
     });
 
-    //Mysql interface
-    const mysqlInterface = new MysqlInterface({
-        connection: mysqlConnection,
-        table: 'testTable'
-    });
-
-    //Model
-    class testModel extends Model {}
-    m = new testModel(mysqlInterface);
-
-    //Migration
-    describe('Model', function() {
-        describe('Raw sql', function() {
-            it('Truncate table', function() {
-                m.execute('TRUNCATE `testTable`');
-                assert.strictEqual(m.all().length, 0);
-            });
-        });
-
-        describe('Insert', function() {
-            it('Should return new count of elements == 1', function() {
-                m.insert({testColumn_first: 'enable', testColumn_second: 'testMe'});
-                assert.strictEqual(m.all().length, 1);
-            });
-    
-            it('Should return new count of elements == 100', function() {
-                for(let i = 0; i < 99; i++) {
-                    m.insert({testColumn_first: 'disable', testColumn_second: 'testMe'});                
-                }
-
-                assert.strictEqual(m.all().length, 100);
-            });
-        });
-
-        describe('Where', function() {
-            it('Should find elements count == 1', function() {
-                assert.strictEqual(m.where([['testColumn_first', `'enable'`]]).length, 1);
-            });
-            it('Should find elements count == 1', function() {
-                assert.strictEqual(m.where([['testColumn_first', '=', `'enable'`]]).length, 1);
-            });
-            it('Should find elements count == 5', function() {
-                assert.strictEqual(m.where([['id', '>=', '1'], ['id', '<=', '5']]).length, 5);
-            });
-        });
-
-        describe('Get', function() {
-            it('Element id should be 5', function() {
-                assert.strictEqual(m.get(5).id, 5);
-            });
-        });
-
-    });
+   
 });
