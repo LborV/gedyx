c67bbf1574e67b5340384bc506515b8813b720ef
Boriss <boriss.djumajevs@1clickgames.com>
2021-07-23 22:36:46 +0300

    docs test revert

--- server/kernel/Action.js
+++ server/kernel/Action.js
@@ -1,25 +1,4 @@
 const Middleware = require('./Middleware');
-
-
-/**
- * Этот класс должны наследовать произвольные Action
- * Аргумент actionName обязателен, он будет использоваться как имя метода для входящий сообщений 
- * 2 и 3 аргументы должны быть обьектами созданными от дочерних классов классов Middleware 
- * <pre><code>
- * socket.on(actionName) 
- * </code></pre>
- * 
- * И также при ответе
- * 
- * <pre><code>
- * socket.emit(actionName)
- * </code></pre>
- * 
- * @param {String} actionName 
- * @param {Array} middlewaresBefore Middleware'ы которые отработают до вызова метода request
- * @param {Array} middlewaresAfter Middleware'ы которые отработают перед отправкой данных
- * @returns {Action}
- */
 class Action {
     constructor(actionName, middlewaresBefore = [], middlewaresAfter = []) {
         this.actionName = actionName;
@@ -28,31 +7,15 @@ class Action {
         return this;
     }
 
-    /**
-     * 
-     * Этот метод вызывается автоматически
-     * @param {Actions} parent 
-     * @returns {Action}
-     */
     setParent(parent) {
         this.parent = parent;
         return this;
     }
 
-    /**
-     * 
-     * Возвращает имя Action
-     * @returns {String}
-     */
     getName() {
         return this.actionName;
     }
 
-    /**
-     * Этот метод вызывается автоматически
-     * @param {Object} data 
-     * @param {socket} socket 
-     */
     requestIn(data, socket) {
         this.socket = socket;
         
@@ -67,30 +30,10 @@ class Action {
         this.request(data);
     }
 
-    /**
-     * 
-     * Этот метод нужно перегрузить в дочернем классе,
-     * он будет вызван на вохдощий запрос
-     * 
-     * <pre><code>
-     * socket.on(actionName) 
-     * </code></pre>
-     * @param {Object} data 
-     */
     request(data) {
         console.log('Request method can be overwriten');
     }
 
-    /**
-     * 
-     * Этот метод применит Middleware'ы и сделает клиенту запрос вроде:
-     * <pre><code>
-     * socket.emit(actionName, data) 
-     * </code></pre>
-     * 
-     * @param {Object} data 
-     * @returns {Object} данные после Middleware'ов 
-     */
     response(data) {
         this.middlewaresAfter.forEach(middleware => {
             if(!(middleware instanceof Middleware)) {
@@ -104,12 +47,6 @@ class Action {
         return data;
     }
 
-    /**
-     * 
-     * Отработает схоже с методом response, только получателем будут все подключеные клиенты
-     * @param {Object} data 
-     * @returns {Object} данные после Middleware'ов 
-     */
     broadcast(data) {
         data = this.response(data);
         this.socket.broadcast.emit(this.actionName, data);
@@ -117,13 +54,6 @@ class Action {
         return data;
     }
 
-    /**
-     * 
-     * Этот метод симулирует запрос клиента в другой Action
-     * @param {String} actionName 
-     * @param {Object} data 
-     * @returns {Object}
-     */
     call(actionName, data) {
         if(actionName === this.actionName) {
             return {};
