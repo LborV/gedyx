8b4ca5c9b348ee0c0daf9c40494797c685bd814d
Boriss <boriss.djumajevs@1clickgames.com>
2022-02-01 17:04:20 +0200

    mysql connection now is pool

--- app/main.js
+++ app/main.js
@@ -16,11 +16,12 @@ async function main() {
             let connection = config.mysql[connectionName];
             if(connection.host && connection.user && connection.user && connection.password && connection.db) {
                 try {
-                    globalThis[connectionName] = await mysql.createConnection({
+                    globalThis[connectionName] = await mysql.createPool({
                         host: connection.host,
                         user: connection.user,
                         password: connection.password,
                         database: connection.db,
+                        connectionLimit: connection.connectionLimit ?? 1
                     });
                 } catch(error) {
                     console.error(error);
